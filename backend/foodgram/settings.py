aa464dcea4924a1b96a60494cb101baaa4eb7016
Артём Большаков <artembolsakov500@gmail.com>
2026-02-06 00:24:18 +0300

    1st iteration fixes v5

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,9 +6,6 @@ from environs import Env
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 env = Env()
 env.read_env()

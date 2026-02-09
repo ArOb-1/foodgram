71f1e6c7b29a44668565c3544d208d538c4b329a
Артём Большаков <artembolsakov500@gmail.com>
2026-02-09 12:27:16 +0300

    Revert "new"
    This reverts commit ce9917113c7b1ad90092f7a7065e8af75c1d11db.

---
+++ backend/recipes/migrations/0002_constraints.py
@@ -0,0 +1,22 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('recipes', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddConstraint(
+            model_name='recipeingredient',
+            constraint=models.UniqueConstraint(fields=('recipe', 'ingredient'), name='unique_recipe_ingredient'),
+        ),
+        migrations.AddConstraint(
+            model_name='favourite',
+            constraint=models.UniqueConstraint(fields=('user', 'recipe'), name='unique_favourite'),
+        ),
+        migrations.AddConstraint(
+            model_name='shoppingcart',
+            constraint=models.UniqueConstraint(fields=('user', 'recipe'), name='unique_shopping_cart'),
+        ),
+    ]
